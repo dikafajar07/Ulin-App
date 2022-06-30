06a93276446b4da933dbea29a1f8ef9f77795365
dikafajar07 <77417628+dikafajar07@users.noreply.github.com>
2022-06-30 12:01:29 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Flutter Dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Campus\\Semester 6\\ABP\\mobile_ulin\\mobile_ulin" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Flutter Dev\\flutter"
+  "PROJECT_DIR=E:\\Campus\\Semester 6\\ABP\\mobile_ulin\\mobile_ulin"
+  "FLUTTER_ROOT=C:\\Flutter Dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Campus\\Semester 6\\ABP\\mobile_ulin\\mobile_ulin\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Campus\\Semester 6\\ABP\\mobile_ulin\\mobile_ulin"
+  "FLUTTER_TARGET=E:\\Campus\\Semester 6\\ABP\\mobile_ulin\\mobile_ulin\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Campus\\Semester 6\\ABP\\mobile_ulin\\mobile_ulin\\.dart_tool\\package_config.json"
+)
